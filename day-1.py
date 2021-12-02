73fe2796f402e90613bf8822e5eed92b9616f2a6
Ronak Shah <ronak.shah@outlook.com>
2021-12-01 23:06:17 -0800

    updated syntax

--- day-1.py
+++ day-1.py
@@ -3,7 +3,6 @@
 with open("day-1-input", 'r') as file: 
     depths = list( map(int, file.readlines()) )
 increases = 0
-window_increases = 0
 
 for i in range(0, len(depths)-1):
     if depths[i] < depths [i+1]:
@@ -12,7 +11,9 @@ for i in range(0, len(depths)-1):
 print (increases)
 
 ## part 2
-for i in range(0, len(depths) - 3):
+window_increases = 0
+
+for i in range(0, len(depths)-3):
     window1 = depths[i] + depths[i+1] + depths[i+2]
     window2 = depths[i+1] + depths[i+2] + depths[i+3]
     if window2 > window1:
